3f9bdb324f579445ec88f490d5b833fc22145a53
StevenNdaye <stevenndaye@gmail.com>
2014-08-25 23:46:48 -0300

    Configured the app

---
+++ movie_crud/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var movies = require('./routes/movies'); //routes are define here
+var app = express();//Create the Express app
+
+var dbName = 'movieDB';
+var connectingString = 'mongodb://localhost:27017/' + dbName;
+
+mongoose.connect(connectionString);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use('/api', movies);
+
+module.exports = app;
+
+
