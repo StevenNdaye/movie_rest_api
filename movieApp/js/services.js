bb3a13311f6b2ea91ed1020a12d08e3eb3743404
StevenNdaye <stevenndaye@gmail.com>
2014-08-27 20:49:31 -0300

    Allowed access from outsiders, completed the app...still to be tested

--- movieApp/js/services.js
+++ movieApp/js/services.js
@@ -4,4 +4,8 @@ angular.module('movieApp.services', []).factory('Movie', function($resource){
       method: 'PUT'
     }
   });
+}).service('popupService', function($window){
+  this.showPopup = function(message){
+    return $window.confirm(message)
+  };
 });
--- movie_crud/app.js
+++ movie_crud/app.js
@@ -11,6 +11,13 @@ mongoose.connect(connectingString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
+app.use(function cors(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use('/api', movies);
 
 module.exports = app;
